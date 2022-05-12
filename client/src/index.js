e19cfd151576c7950e7875106ffcc00b124fca38
kai-commits <kaimeikle123@gmail.com>
2022-05-12 14:36:38 -0700

    refactor reactDOM render

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
