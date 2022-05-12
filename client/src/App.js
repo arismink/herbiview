c2c742e6c5f6afca47406ed3451ab8568e7ac7b8
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-05-12 20:41:17 +0000

    merge react express boilerplate into existing project

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  fetchData = () => {
+    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+    .then((response) => {
+      // handle success
+      console.log(response.data) // The entire response from the Rails API
+
+      console.log(response.data.message) // Just the message
+      this.setState({
+        message: response.data.message
+      });
+    }) 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{ this.state.message }</h1>
+      <button onClick={this.fetchData} >
+        Fetch Data
+      </button>        
     </div>
   );
 }
