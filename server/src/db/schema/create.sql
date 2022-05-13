8e3462c104cea07efe24c9e8121de811ace04d8f
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-05-13 21:57:50 +0000

    add seed data for plant table, add sql tables

--- server/src/db/schema/create.sql
+++ server/src/db/schema/create.sql
@@ -9,18 +9,32 @@ CREATE TABLE users (
   password_digest VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE plants (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  sci_name VARCHAR(255) NOT NULL,
-  common_names TEXT NOT NULL,
-  family VARCHAR(255) NOT NULL, 
-  non_toxic_dogs BOOLEAN NOT NULL, 
-  non_toxic_cats BOOLEAN NOT NULL, 
-  non_toxic_horses BOOLEAN NOT NULL, 
-  clinical_signs TEXT NOT NULL,
-  image_url VARCHAR(255) NOT NULL,
-  aspca_url VARCHAR(255) NOT NULL
+-- CREATE TABLE plants (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   name VARCHAR(255) NOT NULL,
+--   sci_name VARCHAR(255) NOT NULL,
+--   common_names TEXT NOT NULL,
+--   family VARCHAR(255) NOT NULL, 
+--   non_toxic_dogs BOOLEAN NOT NULL, 
+--   non_toxic_cats BOOLEAN NOT NULL, 
+--   non_toxic_horses BOOLEAN NOT NULL, 
+--   clinical_signs TEXT NOT NULL,
+--   image_url VARCHAR(255) NOT NULL,
+--   aspca_url VARCHAR(255) NOT NULL
+-- );
+
+CREATE TABLE IF NOT EXISTS plants (
+    id SERIAL PRIMARY KEY NOT NULL,
+    family VARCHAR(255) NOT NULL,
+    scientific_name VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    non_toxicity_cats BOOLEAN NOT NULL,
+    non_toxicity_dogs BOOLEAN NOT NULL,
+    non_toxicity_horses BOOLEAN NOT NULL,
+    additional_common_names TEXT,
+    clinical_signs TEXT,
+    url VARCHAR(255) NOT NULL,
+    image_url VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE searches (
@@ -28,7 +42,7 @@ CREATE TABLE searches (
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   plant_id INTEGER UNIQUE REFERENCES plants(id) ON DELETE CASCADE,
   common_names TEXT NOT NULL, 
-  desc TEXT NOT NULL, 
+  description TEXT NOT NULL, 
   url VARCHAR(255) NOT NULL,
   image_url VARCHAR(255) NOT NULL,
   edible TEXT NOT NULL, 
