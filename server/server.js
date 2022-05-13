669febf436531e293a22fe4ad61374bbd14448b4
kai-commits <kaimeikle123@gmail.com>
2022-05-12 19:11:41 -0700

    wip move api call to server side

--- server/server.js
+++ server/server.js
@@ -1,24 +1,36 @@
-const Express = require('express');
+const Express = require("express");
 const App = Express();
-const BodyParser = require('body-parser');
-const cors = require('cors');
+const BodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 const PORT = 8080;
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(cors());
-App.use(Express.static('public'));
+App.use(Express.static("public"));
 
 // Sample GET route
-App.get('/api/data', (req, res) => {
+App.get("/api/data", (req, res) => {
   console.log("GET /api/data");
   res.json({
     message: "Seems to work kinda!",
-  })
+  });
+});
+
+App.post("/api/identify", async (req, res) => {
+  await axios.post("https://api.plant.id/v2/identify", req.body)
+  .then(res => {
+    console.log('Success: ', res.data);
+  }).catch(err => {
+    console.log('Error: ', err);
+  });
 });
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
+  console.log(
+    `Express seems to be listening on port ${PORT} so that's pretty good 👍`
+  );
 });
