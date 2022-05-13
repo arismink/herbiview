cce62ded9e3cc237675e0e4c593111e57ad0834b
kai-commits <kaimeikle123@gmail.com>
2022-05-13 10:44:25 -0700

    refactor api call to seperate route

--- client/src/hooks/useAppData.js
+++ client/src/hooks/useAppData.js
@@ -9,7 +9,10 @@ export default function useAppData() {
 
   const sendToPlantAPI = () => {
     convertImageToBase64(state.file).then((base64file) => {
-      axios.post("api/identify", { base64file });
+      axios.post("api/identify", { base64file })
+      .then(res => {
+        console.log(res.data);
+      });
     });
   };
 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
+
 const Express = require("express");
 const App = Express();
 const BodyParser = require("body-parser");
 const cors = require("cors");
-const axios = require("axios");
-require("dotenv").config();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
@@ -12,44 +13,18 @@ App.use(BodyParser.json({ limit: "50mb" }));
 App.use(cors());
 App.use(Express.static("public"));
 
+// Routes
+const identifyRoutes = require("./routes/identify");
+
+// Mount resource routes
+App.use("/api/identify", identifyRoutes());
+
 // Sample GET route
 App.get("/api/data", (req, res) => {
   console.log("GET /api/data");
   res.json({
     message: "Seems to work kinda!",
   });
-});
-
-App.post("/api/identify", (req, res) => {
-  const base64file = req.body.base64file;
-  const data = {
-    images: [base64file],
-    // modifiers docs: https://github.com/flowerchecker/Plant-id-API/wiki/Modifiers
-    modifiers: ["crops_fast", "similar_images"],
-    plant_language: "en",
-    // plant details docs: https://github.com/flowerchecker/Plant-id-API/wiki/Plant-details
-    plant_details: [
-      "common_names",
-      "url",
-      "name_authority",
-      "wiki_description",
-      "taxonomy",
-      "synonyms",
-    ],
-  };
-  const config = {
-    headers: {
-      "Api-Key": process.env.API_KEY,
-    },
-  };
-  axios
-    .post("https://api.plant.id/v2/identify", data, config)
-    .then((res) => {
-      console.log("Success: ", res.data);
-    })
-    .catch((err) => {
-      console.log("Error: ", err);
-    });
 });
 
 App.listen(PORT, () => {
