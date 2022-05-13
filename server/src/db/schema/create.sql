111e0011521644b0ed78343d154405556d3e8662
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-05-13 18:49:15 +0000

    add schema and data template

---
+++ server/src/db/schema/create.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS searches CASCADE;
+DROP TABLE IF EXISTS plants CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password_digest VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE plants (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  sci_name VARCHAR(255) NOT NULL,
+  common_names TEXT NOT NULL,
+  family VARCHAR(255) NOT NULL, 
+  non_toxic_dogs BOOLEAN NOT NULL, 
+  non_toxic_cats BOOLEAN NOT NULL, 
+  non_toxic_horses BOOLEAN NOT NULL, 
+  clinical_signs TEXT NOT NULL,
+  image_url VARCHAR(255) NOT NULL,
+  aspca_url VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE searches (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  plant_id INTEGER UNIQUE REFERENCES plants(id) ON DELETE CASCADE,
+  common_names TEXT NOT NULL, 
+  desc TEXT NOT NULL, 
+  url VARCHAR(255) NOT NULL,
+  image_url VARCHAR(255) NOT NULL,
+  edible TEXT NOT NULL, 
+  date TIMESTAMP
+);
