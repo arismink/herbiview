d663f21d18f8c58c286ff3a744d2acc21cb7f4b4
arismink <krismina.la@gmail.com>
2022-05-13 11:19:48 -0600

    add app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import { Button, Typography } from '@mui/material';
+
 import './App.css';
 import useAppData from './hooks/useAppData';
 
@@ -17,7 +19,8 @@ function App() {
       <h1>Herbiview</h1>
       <form onSubmit={handleSubmit}>
         <input type='file' onChange={handleChange} />
-        <button type='submit'>Upload</button>
+
+        <Button variant="contained" type='submit'>Upload</Button>
       </form> 
 
       <Footer />
