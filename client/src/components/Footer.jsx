4b7f972857cca661d8e32034ca211bbd4daab786
arismink <krismina.la@gmail.com>
2022-05-12 22:09:28 -0600

    add base footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,16 @@
+import Toolbar from '@mui/material/Toolbar';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvira } from '@fortawesome/free-brands-svg-icons';
 
 import "../styles/Footer.scss";
+
+export default function Footer() {
+  return (
+    <div class="main-footer">
+      <div class="logo-footer">
+        <h3><FontAwesomeIcon icon={faEnvira} /> HERBIVIEW</h3>
+      </div>
+    </div>
+  )
+}
