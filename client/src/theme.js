94f5f760dbe034392b2092d8f2e30e008c279f7e
arismink <krismina.la@gmail.com>
2022-05-12 19:45:38 -0600

    add theme with font and color

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,17 +1,29 @@
 import { createTheme } from '@mui/material/styles';
 
+const variables = {
+  primary_bg: '#FFFFFF',
+  primary_font: 'Inter',
+
+  primary_font_color: '#000000'
+};
+
 const theme = createTheme({
   palette: {
     type: 'light',
     primary: {
-      main: '#ffffff',
+      main: variables.primary_bg
     },
     secondary: {
       main: '#f50057',
     },
   },
   typography: {
-    fontFamily: 'Inter',
+    fontFamily: variables.primary_font,
+    h1: {
+      fontFamily: 'Heebo',
+      fontSize: '3rem',
+      fontWeight: 600,
+    },
   },
 });
 
