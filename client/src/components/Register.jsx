94dfa86b6e06c25a18a84c06e772d33ee4c0263a
arismink <krismina.la@gmail.com>
2022-05-13 21:32:01 -0600

    implement react-hook-form

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -2,45 +2,73 @@ import { useForm } from "react-hook-form"
 
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Container, Button } from "@mui/material";
+import { Container, Button, Typography } from "@mui/material";
+
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
 
 export default function Register() {
   const { register, handleSubmit } = useForm();
 
+  // Handle registration with this function
+  const handleRegistration = data => console.log(data);
+
+  const onErrors = errors => console.error(errors);
 
   return (
 <Container maxWidth="md">
-      <h2>Register</h2>
+
+  <PersonAddAltIcon style={{ fontSize: 100, color: 'grey' }}/>
+    <Typography variant="h4" margin={2}>Register</Typography>
 
       <Container
-        component="form"
         sx={{
           width: 500,
           maxWidth: "100%",
           alignItems: "center",
         }}
         noValidate
-        autoComplete="off"
         >
 
-        <form>
+        <form onSubmit={handleSubmit(handleRegistration, onErrors)}
+        autoComplete="off">
 
           <Box mb={2}>
+          <TextField
+              required
+              id="outlined-name-input"
+              label="Name"
+              type="Name"
+              fullWidth
+              sx={{mb: 2}}
+              autoFocus
+              {...register('name', { required: true })}
+            />
+
             <TextField
+              required
               id="outlined-email-input"
               label="Email"
               type="email"
               fullWidth
               sx={{mb: 2}}
+              {...register("email", {
+                required: "Required field",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Invalid email address",
+                },
+              })}
             />
 
             <TextField
+              required
               id="outlined-password-input"
               label="Password"
               type="password"
               autoComplete="current-password"
               fullWidth
               sx={{mb: 2}}
+              {...register('password', { required: true })}
             />
 
           </Box>
@@ -49,7 +77,7 @@ export default function Register() {
             type="submit"
             variant="contained"
             fullWidth
-            sx={{mb: 4}}>Login</Button>
+            sx={{mb: 4}}>Register</Button>
 
         </form>
 
