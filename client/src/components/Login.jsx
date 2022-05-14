eb0736ffd86fe95ba3570c01d0b6e9e21f9d0ed6
arismink <krismina.la@gmail.com>
2022-05-13 18:55:26 -0600

    add link to signup page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
   return (
     <div>
       <h1>LOGIN</h1>
+
+      <Link to="/register">Don't have an account? Register here</Link>
     </div>
   );
 }
