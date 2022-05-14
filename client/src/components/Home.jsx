5dd899db6416ebbb871986b74b12f2e45c924442
arismink <krismina.la@gmail.com>
2022-05-13 22:46:44 -0600

    add hero-image

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,9 @@
+
+
 export default function Home({sendToPlantAPI, setFile}) {
   return (
     <div>
+      <img src="/hero_image.jpeg" alt="hero" />
       <h1>Herbiview</h1>
       <form onSubmit={sendToPlantAPI}>
         <input type="file" onChange={setFile} />
