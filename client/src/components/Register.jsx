97a6e5326cd9ea75e1a0edf6c9fb5453fbf8d305
arismink <krismina.la@gmail.com>
2022-05-13 21:04:01 -0600

    add form component

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,7 +1,60 @@
+import { useForm } from "react-hook-form"
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Container, Button } from "@mui/material";
+
 export default function Register() {
+  const { register, handleSubmit } = useForm();
+
+
   return (
-    <div>
-      <h1>SIGN UP</h1>
-    </div>
+<Container maxWidth="md">
+      <h2>Register</h2>
+
+      <Container
+        component="form"
+        sx={{
+          width: 500,
+          maxWidth: "100%",
+          alignItems: "center",
+        }}
+        noValidate
+        autoComplete="off"
+        >
+
+        <form>
+
+          <Box mb={2}>
+            <TextField
+              id="outlined-email-input"
+              label="Email"
+              type="email"
+              fullWidth
+              sx={{mb: 2}}
+            />
+
+            <TextField
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              fullWidth
+              sx={{mb: 2}}
+            />
+
+          </Box>
+
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            sx={{mb: 4}}>Login</Button>
+
+        </form>
+
+       </Container>
+
+    </Container>
   )
 };
