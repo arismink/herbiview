66153e270e62e48b083da982bbe41e5f305496ed
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-05-14 07:44:32 +0000

    add database seeds

--- server/src/db/schema/create.sql
+++ server/src/db/schema/create.sql
@@ -1,4 +1,5 @@
-DROP TABLE IF EXISTS searches CASCADE;
+DROP TABLE IF EXISTS toxicity CASCADE;
+DROP TABLE IF EXISTS plant_queries CASCADE;
 DROP TABLE IF EXISTS plants CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
 
@@ -9,42 +10,31 @@ CREATE TABLE users (
   password_digest VARCHAR(255) NOT NULL
 );
 
--- CREATE TABLE plants (
---   id SERIAL PRIMARY KEY NOT NULL,
---   name VARCHAR(255) NOT NULL,
---   sci_name VARCHAR(255) NOT NULL,
---   common_names TEXT NOT NULL,
---   family VARCHAR(255) NOT NULL, 
---   non_toxic_dogs BOOLEAN NOT NULL, 
---   non_toxic_cats BOOLEAN NOT NULL, 
---   non_toxic_horses BOOLEAN NOT NULL, 
---   clinical_signs TEXT NOT NULL,
---   image_url VARCHAR(255) NOT NULL,
---   aspca_url VARCHAR(255) NOT NULL
--- );
-
 CREATE TABLE IF NOT EXISTS plants (
-    id SERIAL PRIMARY KEY NOT NULL,
-    family VARCHAR(255) NOT NULL,
-    scientific_name VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    non_toxicity_cats BOOLEAN NOT NULL,
-    non_toxicity_dogs BOOLEAN NOT NULL,
-    non_toxicity_horses BOOLEAN NOT NULL,
-    additional_common_names TEXT,
-    clinical_signs TEXT,
-    url VARCHAR(255) NOT NULL,
-    image_url VARCHAR(255) NOT NULL
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  sci_name VARCHAR(255) NOT NULL,
+  common_names TEXT,
+  family VARCHAR(255) NOT NULL,
+  aspca_url VARCHAR(255) NOT NULL,
+  image_url VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE searches (
+CREATE TABLE plant_queries (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  plant_id INTEGER UNIQUE REFERENCES plants(id) ON DELETE CASCADE,
-  common_names TEXT NOT NULL, 
+  plant_id INTEGER REFERENCES plants(id) ON DELETE CASCADE,
+  sci_name VARCHAR(255) NOT NULL,
   description TEXT NOT NULL, 
-  url VARCHAR(255) NOT NULL,
-  image_url VARCHAR(255) NOT NULL,
-  edible TEXT NOT NULL, 
+  info_url VARCHAR(255) NOT NULL,
+  user_img_url VARCHAR(255) NOT NULL,
+  common_names TEXT NOT NULL, 
   date TIMESTAMP
+);
+
+CREATE TABLE toxicity (
+  id SERIAL PRIMARY KEY NOT NULL,
+  plant_id INTEGER REFERENCES plants(id) ON DELETE CASCADE,
+  animal VARCHAR(255) NOT NULL,
+  toxic BOOLEAN NOT NULL
 );
