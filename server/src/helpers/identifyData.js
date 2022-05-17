4b409045e10259b87bd3065195034264964fad4a
kai-commits <kaimeikle123@gmail.com>
2022-05-17 12:25:25 -0700

    send data to client

--- server/src/helpers/identifyData.js
+++ server/src/helpers/identifyData.js
@@ -6,7 +6,7 @@ const identifyData = (data) => {
     info_url: data.suggestions[0].plant_details.url,
     description: data.suggestions[0].plant_details.wiki_description.value,
     sci_name: data.suggestions[0].plant_details.scientific_name,
-    probability: data.suggestions[0].probability,
+    probability: (data.suggestions[0].probability * 100).toFixed(),
     similar_images: [data.suggestions[0].similar_images[0].url, data.suggestions[0].similar_images[1].url],
   }
 }
