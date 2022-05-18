caffd5767798c0c2ae72e4442619db185652caf6
kai-commits <kaimeikle123@gmail.com>
2022-05-17 23:10:23 -0700

    merge server.js

--- server/src/server.js
+++ server/src/server.js
@@ -28,22 +28,16 @@ const identifyRoutes = require("./routes/identify");
 const usersRoutes = require("./routes/users");
 const plantRoutes = require("./routes/plants");
 const userHistory = require("./routes/user_search_history");
-<<<<<<< HEAD
 const toxicityRoutes = require("./routes/toxicity")
-=======
 const search = require("./routes/search");
->>>>>>> 499958cfece63610c7055007b85966f416297c4a
 
 // Mount resource routes
 App.use("/api/identify", identifyRoutes());
 App.use("/api/plants", plantRoutes(db));
 App.use("/api/users", usersRoutes(db));
 App.use("/api/userHistory", userHistory(db));
-<<<<<<< HEAD
 App.use("/api/toxicity", toxicityRoutes(db));
-=======
 App.use("/api/search", search(db));
->>>>>>> 499958cfece63610c7055007b85966f416297c4a
 
 // Sample GET route
 App.get("/api/data", (req, res) => {
