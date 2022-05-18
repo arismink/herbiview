947e232b5feec44cddc7e0d8b0a4b6e976c5052f
arismink <krismina.la@gmail.com>
2022-05-18 15:43:01 -0600

    modify hook to return all plant data for an array

--- client/src/hooks/useSearchData.js
+++ client/src/hooks/useSearchData.js
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function useSearchData() {
-  const [state, setState] = useState();
+  // const [state, setState] = useState({
+  //   data: {}
+  // });
 
-  const searchApi = (params) => {
-    axios.get(`api/search/${params}`)
-    .then((res) => {
-      setState((prev) => ({ ...prev, data: res.data }));
-    });
-  };
+  // const searchApi = (params) => {
+  //   axios.get(`api/search/${params}`)
+  //   .then((res) => {
+  //     setState((prev) => ({ ...prev, data: res.data }));
+  //   });
+  // };
 
-  return { state, searchApi };
+  const getPlantData = () => {
+    return axios.get("api/search")
+    // .then((res) => {
+    //   console.log('usedata', res.data)
+    //   return res.data
+    // })
+  }
+
+  return { getPlantData };
 }
