1bcd2954010beb8248a01d9e5982d1941999a9ed
arismink <krismina.la@gmail.com>
2022-05-18 17:40:15 -0600

    add api/plant/id details axios get request

--- client/src/hooks/useSearchData.js
+++ client/src/hooks/useSearchData.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+import { useNavigate } from "react-router-dom";
+
 export default function useSearchData() {
-  // const [state, setState] = useState({
-  //   data: {}
-  // });
+  const navigate = useNavigate();
+
+  const [queryPlantDetail, setQueryPlantDetails] = useState({
+    data: {}
+  });
 
   // const searchApi = (params) => {
   //   axios.get(`api/search/${params}`)
@@ -13,10 +17,19 @@ export default function useSearchData() {
   //   });
   // };
 
+  // path for dynamic search bar
+  const getPlantDataDetails = (plant_id) => {
+    axios.get(`api/plants/${plant_id}`)
+    .then((res) => {
+      setQueryPlantDetails((prev) => ({...prev, data: res.data}))
+      navigate("query/plant-details")
+    })
+  }
+
   // get array of plants to populate dynamic search options
   const getPlantData = () => {
     return axios.get("api/search")
   }
 
-  return { getPlantData };
+  return { getPlantData, queryPlantDetail, getPlantDataDetails };
 }
