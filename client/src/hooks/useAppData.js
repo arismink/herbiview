43cdfb3910dccc810e8a555ea956aa51c17c8612
kai-commits <kaimeikle123@gmail.com>
2022-05-18 11:05:34 -0700

    refactor toxicity details

--- client/src/hooks/useAppData.js
+++ client/src/hooks/useAppData.js
@@ -8,37 +8,37 @@ export default function useAppData() {
   const [state, setState] = useState({
     file: {},
     data: {},
-    toxicity: []
+    toxicity: [],
   });
 
   const getToxicityDetails = (plantSciName) => {
     return axios
-    .get(`api/toxicity/${plantSciName}`)
-    .then(res => {
-      console.log('toxicity:', res.data);
+      .get(`api/toxicity/${plantSciName}`)
+      .then((res) => {
+        console.log("toxicity:", res.data);
 
-      setState(prev => ({...prev, toxicity: res.data }))
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  }
+        setState((prev) => ({ ...prev, toxicity: res.data }));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const sendToPlantAPI = (e) => {
     e.preventDefault();
     convertImageToBase64(state.file).then((base64file) => {
-      axios.post("api/identify", { base64file })
-      .then(res => {
-        console.log("data: ", res.data);
+      axios
+        .post("api/identify", { base64file })
+        .then((res) => {
+          console.log("data: ", res.data);
 
-        getToxicityDetails(res.data.sci_name)
-        setState(prev => ({...prev, data: res.data}));
-        navigate("/plant-details");
-      })
-      .catch(err => {
-        console.log(err)
-      })
-
+          setState((prev) => ({ ...prev, data: res.data }));
+          getToxicityDetails(res.data.sci_name);
+          navigate("/plant-details");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     });
   };
 
