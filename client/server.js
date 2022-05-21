fa33c620b521b8e4c4aee4fd935fa1a75870f355
arismink <krismina.la@gmail.com>
2022-05-21 14:35:03 -0600

    add heroku setup files

---
+++ client/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/client'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
