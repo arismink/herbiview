ef51714bb626518c9e3645105a40250306c5425d
arismink <krismina.la@gmail.com>
2022-05-21 00:33:53 -0600

    add clinical signs to query

--- server/src/routes/toxicity.js
+++ server/src/routes/toxicity.js
@@ -4,7 +4,7 @@ module.exports = (db) => {
 
   router.get("/:term", (req, res) => {
     db.query(
-      `SELECT toxicity.animal, toxicity.toxic
+      `SELECT toxicity.animal, toxicity.toxic, toxicity.clinical_signs
       FROM plants
       JOIN toxicity ON plants.id = toxicity.plant_id
       WHERE lower(sci_name) like $1 or lower(common_names) like $1 or lower(name) like $1
@@ -13,6 +13,7 @@ module.exports = (db) => {
     )
       .then((data) => {
         const toxicity_details = data.rows;
+
         res.send(toxicity_details);
       })
       .catch((err) => {
