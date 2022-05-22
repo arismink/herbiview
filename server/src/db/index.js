963d866656daa1278d46434d64022e1ffd4a9a3f
arismink <krismina.la@gmail.com>
2022-05-21 20:07:17 -0600

    modify index.js

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -2,7 +2,10 @@ const pg = require("pg");
 
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL || "",
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
 });
 
 client
